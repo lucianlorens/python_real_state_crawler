8aadb772e439c2d4e274ab101fb53d1b5c924176
lucianlorens <lucianlorens@gmail.com>
2021-10-03 20:18:07 +0100

    :tada: first commit, basic tutorial!

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # realstate-crawler
 Crawler to gather data from real state announces. 
+
+Stack:
+* Python
+* Scrapy
+
+
+
+
+TODO:
+* Add Scrapy
---
+++ tutorial_scrapy/tutorial/tutorial/spiders/quote_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class QuoteSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        page = response.url.split('/')
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
